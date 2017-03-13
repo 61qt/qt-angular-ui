1188b62978df73d74286f2a3444da8a1d7379830
神奇辉 <shenqihui0920@gmail.com>
2017-03-13 14:05:18 +0800

    Lazier

---
+++ src/lazier/_index.js
@@ -0,0 +1,10 @@
+import './lazier.scss';
+
+import angular   from 'angular';
+import Component from './component';
+import Service   from './service';
+
+export default angular.module('qtAngularUi.lazier', [])
+.provider('$lazier', Service)
+.directive('lazier', Component)
+.name;
---
+++ src/lazier/component.js
@@ -0,0 +1,69 @@
+import _        from 'lodash';
+import Template from './lazier.jade';
+
+export default function ($lazier) {
+  'ngInject';
+
+  return {
+    restrict   : 'A',
+    transclude : true,
+    replace    : true,
+    template   : Template,
+    require    : ['^?ngModel'],
+    scope      : {
+      imageSrc: '=?ngModel',
+    },
+    link ($scope, $element, $attrs) {
+      $scope.state = 'idle';
+      $scope.finished = false;
+
+      $scope.errorholder = $attrs.errorholder || $lazier.defaults.errorholder;
+      $scope.placeholder = $attrs.placeholder || $lazier.defaults.placeholder;
+      $scope.imageSrc    = $scope.imageSrc || $attrs.lazierSrc || '';
+
+      if (_.isEmpty($scope.imageSrc)) {
+        return false;
+      }
+
+      $scope.onload = function (callback) {
+        let element       = $element[0];
+        let { top, left } = element.getBoundingClientRect();
+
+        if ($scope.finished) {
+          return true;
+        }
+
+        if (top > -element.clientHeight && top < window.innerHeight
+         && left > -element.clientWidth && left < window.innerWidth) {
+          $scope.state = 'loading';
+
+          let image = new Image();
+
+          image.onload = function () {
+            $scope.state = 'success';
+            $scope.finished = true;
+
+            _.isFunction(callback) && callback(null, $scope.imageSrc, $scope);
+
+            $scope.$digest();
+          };
+
+          image.onerror = function (error) {
+            $scope.state = 'error';
+            $scope.finished = true;
+            _.isFunction(callback) && callback(error, $scope.imageSrc, $scope);
+
+            $scope.$digest();
+          };
+
+          image.src = $scope.imageSrc;
+          return true;
+        }
+
+        return false;
+      };
+
+      $lazier.bind($scope);
+    },
+  };
+}
