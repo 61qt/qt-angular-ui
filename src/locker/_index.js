a386aa09116d1d8b9381dde278cbc0b4700187e2
神奇辉 <shenqihui0920@gmail.com>
2017-03-13 18:19:56 +0800

    Locker ，触发时候自动增加 locker

--- src/locker/_index.js
+++ src/locker/_index.js
@@ -5,4 +5,33 @@ import Component from './component';
 export default angular.module('qtAngularUi.locker', [])
 .provider('$locker', Service)
 .directive('locker', Component)
+/**
+ * 处理页面通用事件
+ * 1. 每次跳转的时候: spinner 自动弹出, 导航自动隐藏
+ * 2. 当加载完成后 spinner 自动隐藏, 导航显示
+ * 3. 跳转时候, 滚动条自动回到最上方
+ */
+.run(function ($rootScope, $state, $locker) {
+  'ngInject';
+
+  $rootScope.$on('$stateChangeStart', (event, toState, toParams) => {
+    $locker.show('努力加载中');
+
+    if (toState.redirectTo) {
+      event.preventDefault();
+
+      $state.go(toState.redirectTo, toParams, {
+        location: 'replace',
+      });
+    }
+  });
+
+  $rootScope.$on('$viewContentLoaded', () => {
+    $locker.hide();
+
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 0);
+  });
+})
 .name;
