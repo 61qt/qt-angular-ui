4578597f9fc0fedaa3e2c169f672822122128592
神奇辉 <shenqihui0920@gmail.com>
2017-03-13 19:27:08 +0800

    angular.device

---
+++ src/device/_index.js
@@ -0,0 +1,22 @@
+import _            from 'lodash';
+import MobileDetect from 'mobile-detect';
+import angular      from 'angular';
+
+_.defaultsDeep(MobileDetect, {
+  _impl: {
+    mobileDetectRules: {
+      props: {
+        WeChat     : [/MicroMessenger\/([\d\.]+)/i],
+        BadAndroid : [/Android /i],
+      },
+      uas: {
+        WeChat     : /MicroMessenger\/([\d\.]+)/i,
+        BadAndroid : /Android /i,
+      },
+    },
+  },
+});
+
+angular.device = new MobileDetect(window.navigator.userAgent);
+
+export default angular.module('qtAngularUi.devide', []);
