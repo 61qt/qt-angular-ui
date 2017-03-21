d7e123b93f4fe8aaac91bf193dd8452e9ff7e181
神奇辉 <shenqihui0920@gmail.com>
2017-03-21 11:09:35 +0800

    Env console

---
+++ src/env/index.js
@@ -0,0 +1,7 @@
+if (window.angular && window.angular.env && window.angular.env.QT_UI_LOG) {
+  window.console.log('qt-angular-ui/src/env load');
+}
+
+import component   from './_index';
+
+export default component;
