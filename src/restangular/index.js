a412a186cd30045996cf385a86cdf772bc6fca99
神奇辉 <shenqihui0920@gmail.com>
2017-03-21 16:04:21 +0800

    Add console.log

--- src/restangular/index.js
+++ src/restangular/index.js
@@ -1,3 +1,7 @@
+if (window.angular && window.angular.env && window.angular.env.QT_UI_LOG) {
+  window.console.log('qt-angular-ui/src/restangular load');
+}
+
 import component   from './_index';
 
 export default component;
