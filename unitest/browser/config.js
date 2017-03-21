ffd2f77574319c5852bbbd4d5b153e69e3cb2edd
神奇辉 <shenqihui0920@gmail.com>
2017-03-21 11:17:18 +0800

    remove unitest

--- unitest/browser/config.js
+++
@@ -1,8 +0,0 @@
-import path       from 'path';
-import * as PATHS from '../conf/config';
-
-export const PROJECT_NAME     = 'test';
-export const TEMPORARY_FOLDER = path.join(PATHS.EXEC_PATH, PATHS.TEMPORARY_FOLDER_NAME, 'test_browser');
-export const PROJECT_FOLDER   = path.join(TEMPORARY_FOLDER, './project');
-export const MODULE_FOLDER    = path.join(TEMPORARY_FOLDER, './module');
-export const COMPONENT_FOLDER = path.join(TEMPORARY_FOLDER, './component');
